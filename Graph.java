f3a1431aaf45f5556bc63d17ab0481c9d671aa1c
Lewis Kim <lewis.k@berkeley.edu>
2018-09-21 00:01:14 -0700

    Implemented weighted undirected graphs (UndirectedGraph()).

--- Graph.java
+++ Graph.java
@@ -1,28 +1,107 @@
-// A Graph representation using an adjacency list, used for Networkify/.
+// Undirected and Directed Graphs using an adjacency lists; used for Networkify.
 //
 // Written by Lewis Kim.
 
+import java.util.HashTable;
 
+
+// Class of a weighted undirected graph using adjacency list representation through HashTables.
 static class UndirectedGraph {
 
-	int V;
-	LinkedList<Integer> adjacencyList[];
+	/** Adjacency list using a hashmap of hashmaps to represent a vertex and all of its edges/edge weights.
 
-	// Constructor.
-	Graph(int V) {
-		this.V = V;
+		Example:
+		{"v1": {"v2":2, "v3" 2}, "v2": {"v1": }, ...} **/
+	HashMap<String, HashMap<String, Double>> adjacencyList;
 
-		adjacencyList = new LinkedList[V];
 
-		for(int i = 0; i < V; i++) {
-			adjacencyList[i] = new LinkedList<>();
+	public UndirectedGraph(T root) {
+		this.adjacencyList = new HashMap<>();
+	}
+
+
+	/** Add a vertex V to this graph. **/
+	public void addVertex(String v) {
+		if (this.adjacencyList.containsKey(v)) {
+			throw new IllegalArgumentException("This graph already has that vertex.");
 		}
+
+		this.adjacencyList.put(v, new HashMap<String, Double>());
 	}
 
-	// Add an edge in GRAPH from U to V.
-	static void addEdge(Graph graph, int u, int v) {
-		
-		graph.adjacencyList[u].addFirst(v);
-		graph.adjacencyList[v].addFirst(u);
+
+	/** Remove a vertex V and ALL of its associated edges. **/
+	public void removeVertex(String v) {
+		if (this.adjacencyList.containsKey(v)) {
+			throw new IllegalArgumentException("This graph already has that vertex.");
+		}
+
+		for (String vertex : this.adjacencyList.entrySet()) {
+			vertex.remove(v)
+		}
+
+		this.adjacencyList.remove(v)
 	}
+
+
+	/** Add an edge between vertices V and U, with weight WEIGHT. **/
+	public void addEdge(String v, String u, Double weight) {
+		if (!this.adjacencyList.containsKey(v)) {
+			throw new IllegalArgumentException("This graph does not contain the vertex " + v + ".");
+		} else if (!this.adjacencyList.containsKey(u)) {
+			throw new IllegalArgumentException("This graph does not contain the vertex " + u + ".");
+		}
+
+		this.adjacencyList.get(v).put(u, weight);
+		this.adjacencyList.get(u).put(v, weight);
+	}
+
+
+	/** Remove an edge between vertex V and U. **/
+	public void removeEdge(String v, String u, Double weight) {
+		if (!this.adjacencyList.containsKey(v)) {
+			throw new IllegalArgumentException("This graph does not contain the vertex " + v + ".");
+		} else if (!this.adjacencyList.containsKey(u)) {
+			throw new IllegalArgumentException("This graph does not contain the vertex " + u + ".");
+		}
+
+		this.adjacencyList.get(v).remove(u);
+		this.adjacencyList.get(u).remove(v);
+	}
+
+
+	/** Replace the edge weight between vertex V and U with new weight, WEIGHT. **/
+	public void replaceWeight(String v, String u, Double weight) {
+		if (!this.adjacencyList.containsKey(v)) {
+			throw new IllegalArgumentException("This graph does not contain the vertex " + v + ".");
+		} else if (!this.adjacencyList.containsKey(u)) {
+			throw new IllegalArgumentException("This graph does not contain the vertex " + u + ".");
+		}
+
+		this.adjacencyList.get(v).replace(u, weight);
+		this.adjacencyList.get(u).replace(v, weight);
+	}
+
+
+	/** Print the current graph. **/
+	public void printGraph() {
+
+	}
+
+
+
+	// Various Algorithms.
+
+
 }
+
+
+static class DirectedGraph {
+
+
+
+}
+
+
+
+
